acccee93bc91415671424079e3dde6a0682457f4
Ficus22 <130400795+Ficus22@users.noreply.github.com>
2025-03-16 19:32:39 +0100

    7 points :(

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -2,70 +2,114 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_NODE_PORT } from "../config";
 import { Value } from "../types";
+import { broadcastMessage, delay } from "../utils";
 
 export async function node(
-  nodeId: number, // the ID of the node
-  N: number, // total number of nodes in the network
-  F: number, // number of faulty nodes in the network
-  initialValue: Value, // initial value of the node
-  isFaulty: boolean, // true if the node is faulty, false otherwise
-  nodesAreReady: () => boolean, // used to know if all nodes are ready to receive requests
-  setNodeIsReady: (index: number) => void // this should be called when the node is started and ready to receive requests
+  nodeId: number,
+  N: number,
+  F: number,
+  initialValue: Value,
+  isFaulty: boolean,
+  nodesAreReady: () => boolean,
+  setNodeIsReady: (index: number) => void
 ) {
   const node = express();
   node.use(express.json());
   node.use(bodyParser.json());
 
   type NodeState = {
-    killed: boolean; // this is used to know if the node was stopped by the /stop route. It's important for the unit tests but not very relevant for the Ben-Or implementation
-    x: 0 | 1 | "?" | null; // the current consensus value
-    decided: boolean | null; // used to know if the node reached finality
-    k: number | null; // current step of the node
+    killed: boolean;
+    x: 0 | 1 | "?" | null;
+    decided: boolean | null;
+    k: number | null;
   };
 
-   // État initial du nœud
-   let nodeState: NodeState = {
+  let nodeState: NodeState = {
     killed: false,
     x: isFaulty ? null : initialValue,
     decided: isFaulty ? null : false,
-    k: isFaulty ? null : 0
+    k: isFaulty ? null : 0,
   };
 
-  // TODO implement this
-  // this route allows retrieving the current status of the node
+  let receivedMessages: { step: number; value: Value }[] = [];
+
   node.get("/status", (req, res) => {
-    if (isFaulty) {
-      res.status(500).send("faulty");
-    } else {
-      res.status(200).send("live");
-    }
+    res.status(isFaulty ? 500 : 200).send(isFaulty ? "faulty" : "live");
   });
 
-  // TODO implement this
-  // this route allows the node to receive messages from other nodes
-  // node.post("/message", (req, res) => {});
-
-  // TODO implement this
-  // this route is used to start the consensus algorithm
-  // node.get("/start", async (req, res) => {});
-
-  // TODO implement this
-  // this route is used to stop the consensus algorithm
-  // node.get("/stop", async (req, res) => {});
-
-  // TODO implement this
-  // get the current state of a node
   node.get("/getState", (req, res) => {
     res.json(nodeState);
   });
 
-  // start the server
-  const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
-    console.log(
-      `Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`
-    );
+  node.get("/start", async (req, res) => {
+    if (nodeState.killed) {
+      res.status(500).send("Node stopped");
+      return;
+    }
+  
+    while (!nodesAreReady()) {
+      await delay(100);
+    }
+  
+    let step = 0;
+  
+    // Initialisation : envoi des messages aux autres nœuds sans attendre de réponse
+    broadcastMessage(nodeId, N, step, nodeState);
+  
+    res.send("Initialization complete, waiting for consensus...");
+  });
+  
+  node.post("/message", (req, res) => {
+    if (nodeState.killed) {
+      res.status(500).send("Node stopped");
+      return;
+    }
+  
+    const { step, value } = req.body;
+  
+    if (!isFaulty && nodeState.k === step) {
+      receivedMessages.push({ step, value });
+    }
+  
+    if (receivedMessages.length === N - 1) {
+      const voteCounts: Record<string, number> = {};
+  
+      receivedMessages.forEach(({ value }) => {
+        if (value !== "?") {
+          voteCounts[value] = (voteCounts[value] || 0) + 1;
+        }
+      });
+  
+      const majorityValue = Object.keys(voteCounts).length
+        ? Object.keys(voteCounts).reduce((a, b) =>
+            voteCounts[a] > voteCounts[b] ? a : b
+          )
+        : null;
+  
+      if (majorityValue !== null && voteCounts[majorityValue] > (N - F) / 2) {
+        nodeState.x = parseInt(majorityValue, 10) as Value; // Convertir en nombre
+        nodeState.decided = true;
+      }
+  
+      // On passe à l'étape suivante
+      if (nodeState.k !== null) {
+        nodeState.k++;
+      } else {
+        nodeState.k = 1; // Initialiser à 1 si null
+      }
+    }
+  
+    res.sendStatus(200);
+  });
+  
 
-    // the node is ready
+  node.get("/stop", (req, res) => {
+    nodeState.killed = true;
+    res.send("Node stopped");
+  });
+
+  const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
+    console.log(`Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`);
     setNodeIsReady(nodeId);
   });
 
